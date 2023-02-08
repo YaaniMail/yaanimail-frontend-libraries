6e190503602ffb7fa32fe2ecd752351f8c44724e
YAGIZ OZTURK <yagiz.ozturk@turkcell.com.tr>
2023-02-08 17:28:32 +0300

    no message

--- projects/ngym-profile-photo/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngym-profile-photo
- */
-
-export * from './lib/ngym-profile-photo.service';
-export * from './lib/ngym-profile-photo.component';
-export * from './lib/ngym-profile-photo.module';
