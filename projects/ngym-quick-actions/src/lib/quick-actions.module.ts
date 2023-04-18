e7a7846a9d6546ad07372cb9969ccad18ce18ae8
nilseri01 <nilseri@gmail.com>
2023-04-18 20:59:32 +0300

    PGN-5034 quick actions library

--- projects/ngym-quick-actions/src/lib/quick-actions.module.ts
+++ projects/ngym-quick-actions/src/lib/quick-actions.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuickActionsComponent } from './quick-actions.component';
+import { NgymDropdownModule } from 'ngym-dropdown';
 
 @NgModule({
   declarations: [
     QuickActionsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgymDropdownModule
   ],
   exports: [
     QuickActionsComponent
