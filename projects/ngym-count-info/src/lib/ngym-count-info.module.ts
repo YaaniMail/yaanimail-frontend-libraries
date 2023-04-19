c06de1d39eb47b5b4791fc1b64a3f01951d5ef55
nilseri01 <nilseri@gmail.com>
2023-04-19 17:59:00 +0300

    PGN-5040 count-info library

---
+++ projects/ngym-count-info/src/lib/ngym-count-info.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CountInfoComponent } from './count-info.component';
+
+@NgModule({
+  declarations: [
+    CountInfoComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CountInfoComponent
+  ]
+})
+export class NgymCountInfoModule { }
