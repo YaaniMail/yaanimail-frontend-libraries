39a1218fef3e51bd76e498b3ef962af6771d0e6c
TURKCELL\EXT02D53281 <anil.ozen@consultant.turkcell.com.tr>
2023-05-18 13:30:14 +0300

    country code support added

--- projects/ngym-contacts/src/lib/pipe/phone-formatter.pipe.ts
+++ projects/ngym-contacts/src/lib/pipe/phone-formatter.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { MaskPipe } from "ngx-mask";
-import parsePhoneNumber from 'libphonenumber-js'
+import parsePhoneNumber from 'libphonenumber-js';
 
 @Pipe({
   name: 'phoneFormatter'
@@ -16,30 +16,38 @@ export class PhoneFormatterPipe implements PipeTransform {
     const phoneNumber = parsePhoneNumber(value);
     if (phoneNumber){
       const formattedNumber = phoneNumber.formatInternational().split(' ');
-      formattedNumber[1] = `(${formattedNumber[1]})`
+      formattedNumber[1] = formattedNumber.length > 3 ? `(${formattedNumber[1]})`: formattedNumber[1];
       return formattedNumber.join(' ');
     }
 
 
     let numbers = value.replace(/\D+/g,'');
+    let pattern='';
+    let hasCountryCode = false;
     const code = numbers.slice(0,-7);
 
-    let pattern='';
 
     if (!code){
       pattern = '(000)';
-    } else if(code.length === 1){
+    } else if(code.length === 1 || code.length === 2){
       pattern = '(0000)';
     } else if (code.length === 3){
       pattern = '(000) 000'
     } else if (code.length == 4){
       pattern = '(0000) 000';
+    } else if (code.length > 4) {
+      hasCountryCode = true;
+      for (let i = 0; i< code.length-3;i++) {
+        pattern += '0'
+      }
+      pattern += ' (000) 000'
     } else {
       return value;
     }
 
     pattern += ' 00 00';
-    return this.maskPipe.transform(numbers, pattern);
+
+    return `${hasCountryCode?'+':''}${this.maskPipe.transform(numbers, pattern)}`;
 
   }
 
