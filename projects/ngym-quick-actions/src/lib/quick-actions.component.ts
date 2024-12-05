5915bf5289a609e77d1eb18da489ae248da2ba88
nilseri01 <senoritadeveloper01@gmail.com>
2024-12-05 19:33:59 +0300

    scroll listener added to quick actions

--- projects/ngym-quick-actions/src/lib/quick-actions.component.ts
+++ projects/ngym-quick-actions/src/lib/quick-actions.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from
 import { ViewDropDownQuickAction } from './model/view-dropdown-quick-action';
 import { ViewIconQuickAction } from './model/view-icon-quick-action';
 import { DropDownItem } from 'ngym-dropdown';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ngym-quick-actions',
   templateUrl: './quick-actions.component.html',
-  styleUrls: ['./quick-actions.component.scss']
+  styleUrls: ['./quick-actions.component.scss'],
 })
 export class QuickActionsComponent implements OnChanges {
   iconActions: (ViewIconQuickAction | ViewDropDownQuickAction)[] = [];
@@ -24,39 +25,49 @@ export class QuickActionsComponent implements OnChanges {
   @Input() dropdownPlacement: string = '';
   @Input() dropdownContainer: string = '';
   @Input() actions: (ViewIconQuickAction | ViewDropDownQuickAction)[] = [];
-  @Input() iconStdClasses!: { primary: string, disabled: string };
-  @Input() textStdClasses!: { primary: string, disabled: string };
+  @Input() iconStdClasses!: { primary: string; disabled: string };
+  @Input() textStdClasses!: { primary: string; disabled: string };
+  @Input({ required: false }) scrollEventsObservable?: Observable<void>;
   @Output() actionEmitter = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() {}
 
   ngOnChanges(changes: SimpleChanges): void {
     this.prepareActions();
   }
 
   prepareActions(): void {
-    this.iconActions = this.actions.filter(action => action instanceof ViewIconQuickAction);
-    this.dropDownActions = this.actions.filter(action => action instanceof ViewDropDownQuickAction);
+    this.iconActions = this.actions.filter(
+      (action) => action instanceof ViewIconQuickAction
+    );
+    this.dropDownActions = this.actions.filter(
+      (action) => action instanceof ViewDropDownQuickAction
+    );
     this.prepareDropdownActions();
   }
 
   prepareDropdownActions(): void {
     this.dropDownItems = [];
-    this.dropDownActions.forEach(action => {
-      this.dropDownItems.push(
-        {
-          robotClass: action.robotClass,
-          label: action.label,
-          action: action,
-          linkClasses: !(action.isActionAllowed) ? this.textStdClasses.disabled : '',
-          isLoadingClasses: action.checkIsLoading ? this.textStdClasses.disabled : ''
-        }
-      );
+    this.dropDownActions.forEach((action) => {
+      this.dropDownItems.push({
+        robotClass: action.robotClass,
+        label: action.label,
+        action: action,
+        linkClasses: !action.isActionAllowed
+          ? this.textStdClasses.disabled
+          : '',
+        isLoadingClasses: action.checkIsLoading
+          ? this.textStdClasses.disabled
+          : '',
+      });
     });
   }
 
-  performDropdownAction(action: (ViewIconQuickAction | ViewDropDownQuickAction)) {
-    if (action.isActionAllowed && (!action.checkIsLoading || (action.checkIsLoading && !this.isLoading)))
+  performDropdownAction(action: ViewIconQuickAction | ViewDropDownQuickAction) {
+    if (
+      action.isActionAllowed &&
+      (!action.checkIsLoading || (action.checkIsLoading && !this.isLoading))
+    )
       this.actionEmitter.emit(action.actionType);
   }
 }
